2bd9739c189c63689d3a368fdef691405a1c5e85
Gautam0143 <80623080+Gautam0143@users.noreply.github.com>
2023-11-29 23:21:07 -0600

    Update passenger-and-flight.service.ts

--- src/app/service/passenger-and-flight.service.ts
+++ src/app/service/passenger-and-flight.service.ts
@@ -9,7 +9,7 @@ export class PassengerAndFlightService {
 
 constructor(private http: HttpClient) { }
 
-  private passengerBaseUrl = 'http://localhost:6006';
+  private passengerBaseUrl = 'https://frontendflightproject.uc.r.appspot.com';
    public getData(username: string, password: string): Observable<any> {
        // Use template literals to interpolate variables into the URL string
        return this.http.get<any>(`${this.passengerBaseUrl}/findByPassengerUsernameAndPassengerPassword?username=${username}&password=${password}`);
